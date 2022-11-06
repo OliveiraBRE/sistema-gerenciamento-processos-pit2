d17324aeb30c40354767e8affe1aa1f57b3c21a3
OliveiraBRE <bruno.r.e.oliveira@gmail.com>
2022-11-05 22:06:59 -0300

    fix: file name -> url path compatibility

--- src/controller/ManagerProjectController.js
+++ src/controller/ManagerProjectController.js
@@ -4,15 +4,10 @@ const pathDir = path.resolve('database');
 
 module.exports = {
   buildingProjectFileName(projectName) {
-    const lowerName = projectName.toLowerCase().split(' ');
-    
-    const name = lowerName.map(word => {
-      const letters = word.split('');
-      const firstLetter = letters.shift().toUpperCase();
-      const projectName = [firstLetter, ...letters].join('');
-
-      return projectName;
-    }).join('').normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+    const name = projectName
+      .toLowerCase()
+      .replace(' ', '-')
+      .normalize('NFD').replace(/[\u0300-\u036f]/g, "");
 
     return `${name}.json`;
 
